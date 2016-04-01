9880636d197d68c5d8f41d0f684f88c7904e7b63
dmartana <daniel.martana@greenwavereality.com>
2016-04-01 17:56:17 +0800

    Enhancement:
    1. larger data set
    2. randomization with time.Now
    3. calculate total duration
    4. added 1 sec sleep if down

--- multiping.go
+++ multiping.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"sync"
+	"time"
 )
-
-const maxConcurrency = 6 //default
 
 func ping(wg *sync.WaitGroup, host string) {
 	defer wg.Done()
@@ -14,19 +13,74 @@ func ping(wg *sync.WaitGroup, host string) {
 	//temporary pseudo logic of ping
 	if rand.Int()%2 == 1 {
 		fmt.Println(host, "is down")
+		time.Sleep(1000 * time.Millisecond) //sleep 1 sec to simulate unreachable host
 	} else {
 		fmt.Println(host, "is up")
 	}
 }
 
 func main() {
-	rand.Seed(42)
+	startTime := time.Now().UTC()
+
+	rand.Seed(time.Now().UnixNano() / (int64(time.Millisecond)/int64(time.Nanosecond)))
 	var wg sync.WaitGroup
 
-	hosts := []string{"q", "w", "e", "r", "t", "y"}
+	hosts := []string{	"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y",
+						"q", "w", "e", "r", "t", "y" }
 	for i := 0; i < len(hosts); i++ {
 		wg.Add(1)
 		go ping(&wg, hosts[i])
 	}
 	wg.Wait()
+
+	endTime := time.Now().UTC()
+	fmt.Println(endTime.Sub(startTime))
 }
